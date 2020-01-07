bf29f5dc8d3b593eb2d1f43a536cf4a2a8faf4d2
m1t9 <dpleshanov@gmail.com>
2020-01-07 20:49:16 +0300

    3109 update

--- task31/task3109/Solution.java
+++ task31/task3109/Solution.java
@@ -2,7 +2,6 @@ package com.javarush.task.task31.task3109;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.Properties;
 
