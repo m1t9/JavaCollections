8ad9790019a5663baead459249815dd333d8fc85
m1t9 <dpleshanov@gmail.com>
2020-01-07 20:48:40 +0300

    3109

---
+++ task31/task3109/Solution.java
@@ -0,0 +1,37 @@
+package com.javarush.task.task31.task3109;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+/* 
+Читаем конфиги
+*/
+public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        Properties properties = solution.getProperties("4.JavaCollections/src/com/javarush/task/task31/task3109/properties.xml");
+        properties.list(System.out);
+
+        properties = solution.getProperties("4.JavaCollections/src/com/javarush/task/task31/task3109/properties.txt");
+        properties.list(System.out);
+
+        properties = solution.getProperties("4.JavaCollections/src/com/javarush/task/task31/task3109/notExists");
+        properties.list(System.out);
+    }
+
+    public Properties getProperties(String fileName) {
+        try {
+            Properties properties = new Properties();
+            if (fileName.endsWith(".xml")) {
+                properties.loadFromXML(new FileInputStream(new File(fileName)));
+            } else {
+                properties.load(new FileInputStream(new File(fileName)));
+            }
+            return properties;
+        } catch (IOException ignored) { }
+        return new Properties();
+    }
+}
