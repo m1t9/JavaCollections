9122ca1b0d7254a5681ae98e6a392ae92ec0e1ee
m1t9 <dpleshanov@gmail.com>
2020-01-08 19:54:34 +0300

    3213

---
+++ task32/task3213/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task32.task3213;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
+
+/* 
+Шифр Цезаря
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        StringReader reader = new StringReader("Khoor#Dpljr#&C,₷B'3");
+        System.out.println(decode(reader, -3));  //Hello Amigo #@)₴?$0
+    }
+
+    public static String decode(StringReader reader, int key) throws IOException {
+        if (reader == null) return "";
+        BufferedReader bufferedReader = new BufferedReader(reader);
+        StringWriter stringWriter = new StringWriter();
+        for (byte c : bufferedReader.readLine().getBytes()) {
+            stringWriter.write(c + key);
+        }
+        return stringWriter.toString();
+    }
+}
