df637577a59f151f6f9d7a3188a3625ab0bfc185
m1t9 <dpleshanov@gmail.com>
2020-02-01 13:41:28 +0300

    tetris 3/17

--- task22/task2213/Tetris.java
+++ task22/task2213/Tetris.java
@@ -5,6 +5,22 @@ public class Tetris {
     private Field field;
     private Figure figure;
 
+    public Field getField() {
+        return field;
+    }
+
+    public Figure getFigure() {
+        return figure;
+    }
+
+    public void setField(Field field) {
+        this.field = field;
+    }
+
+    public void setFigure(Figure figure) {
+        this.figure = figure;
+    }
+
     public static void main(String[] args) {
         
     }
