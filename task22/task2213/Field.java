0e5207cda9e3b70549204fb2a72789aea48e5731
m1t9 <dpleshanov@gmail.com>
2020-02-01 16:13:09 +0300

    tetris 14/17

--- task22/task2213/Field.java
+++ task22/task2213/Field.java
@@ -1,12 +1,19 @@
 package com.javarush.task.task22.task2213;
 
-public class Field {
+import java.util.Arrays;
 
+/**
+ * Класс Field описывает "поле клеток" игры Тетрис
+ */
+public class Field {
+    //ширина и высота
     private int width;
     private int height;
+
+    //матрица поля: 1 - клетка занята, 0 - свободна
     private int[][] matrix;
 
-    public Field(int width, int height) {
+    public Field(int height, int width) {
         this.width = width;
         this.height = height;
         matrix = new int[height][width];
@@ -24,28 +31,87 @@ public class Field {
         return matrix;
     }
 
-    public void print() {
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
-                if (matrix[y][x] == 0) {
-                    System.out.print(".");
-                } else {
-                    System.out.print("X");
-                }
-            }
-            System.out.print("\n");
-        }
-    }
-
-    public void removeFullLines() {
-
-    }
-
+    /**
+     * Метод возвращает значение, которое содержится в матрице с координатами (x,y)
+     * Если координаты за пределами матрицы, метод возвращает null.
+     */
     public Integer getValue(int x, int y) {
-        return (Integer)matrix[x][y];
+        if (x >= 0 && x < width && y >= 0 && y < height)
+            return matrix[y][x];
+
+        return null;
     }
 
+    /**
+     * Метод устанавливает переданное значение(value) в ячейку матрицы с координатами (x,y)
+     */
     public void setValue(int x, int y, int value) {
-        matrix[x][y] = value;
+        if (x >= 0 && x < width && y >= 0 && y < height)
+            matrix[y][x] = value;
+    }
+
+    /**
+     * Метод печатает на экран текущее содержание матрицы
+     */
+    public void print() {
+        //Создаем массив, куда будем "рисовать" текущее состояние игры
+        int[][] canvas = new int[height][width];
+
+        //Копируем "матрицу поля" в массив
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                canvas[i][j] = matrix[i][j];
+            }
+        }
+
+        //Копируем фигурку в массив, только непустые клетки
+        int left = Tetris.game.getFigure().getX();
+        int top = Tetris.game.getFigure().getY();
+        int[][] brickMatrix = Tetris.game.getFigure().getMatrix();
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (top + i >= height || left + j >= width) continue;
+                if (brickMatrix[i][j] == 1)
+                    canvas[top + i][left + j] = 2;
+            }
+        }
+
+
+        //Выводим "нарисованное" на экран, но начинаем с "границы кадра".
+        System.out.println("---------------------------------------------------------------------------\n");
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                int index = canvas[i][j];
+                if (index == 0)
+                    System.out.print(" . ");
+                else if (index == 1)
+                    System.out.print(" X ");
+                else if (index == 2)
+                    System.out.print(" X ");
+                else
+                    System.out.print("???");
+            }
+            System.out.println();
+        }
+
+
+        System.out.println();
+        System.out.println();
+    }
+
+    /**
+     * Удаляем заполненные линии
+     */
+    public void removeFullLines() {
+        for (int line = 0; line < height; line++) {
+            if (!Arrays.asList(matrix[line]).contains(0)) {
+                for (int lineCopy = line - 1; lineCopy >= 0; lineCopy--) {
+                    matrix[lineCopy + 1] = matrix[lineCopy];
+                }
+                matrix[0] = new int[width];
+            }
+        }
     }
 }
