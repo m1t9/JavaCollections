b36fca8cac227e78dfb148de060b3571519ad4ba
m1t9 <dpleshanov@gmail.com>
2020-02-01 14:50:00 +0300

    tetris 7/17

--- task22/task2213/Field.java
+++ task22/task2213/Field.java
@@ -23,4 +23,20 @@ public class Field {
     public int[][] getMatrix() {
         return matrix;
     }
+
+    public void print() {
+
+    }
+
+    public void removeFullLines() {
+
+    }
+
+    public Integer getValue(int x, int y) {
+        return (Integer)matrix[x][y];
+    }
+
+    public void setValue(int x, int y, int value) {
+        matrix[x][y] = value;
+    }
 }
