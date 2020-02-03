6d0ab29f3f882d8a6b369f52506c2a1930112f62
m1t9 <dpleshanov@gmail.com>
2020-02-03 19:24:53 +0300

    space 12 13

--- task25/task2515/SpaceShip.java
+++ task25/task2515/SpaceShip.java
@@ -1,7 +1,36 @@
 package com.javarush.task.task25.task2515;
 
 public class SpaceShip extends BaseObject{
-    public SpaceShip(double x, double y, double radius) {
-        super(x, y, radius);
+
+    private double dx = 0;
+
+    public SpaceShip(double x, double y) {
+        super(x, y, 3);
+    }
+
+    public void moveLeft() {
+        dx = -1;
+    }
+
+    public void moveRight() {
+        dx = 1;
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+
+    }
+
+    @Override
+    public void move() {
+        x += dx;
+        this.checkBorders(2, Space.game.getWidth() - 2, 2, Space.game.getHeight() - 2);
+    }
+
+    public void fire() {
+        Rocket rocketLeft = new Rocket(x - 2, y);
+        Space.game.getRockets().add(rocketLeft);
+        Rocket rocketRight = new Rocket(x + 2, y);
+        Space.game.getRockets().add(rocketRight);
     }
 }
